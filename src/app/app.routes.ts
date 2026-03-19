54b45b87121255d8dba596dc461d8193c00d11b3
manuelgarciallera <manuelgarciallera@outlook.com>
2026-03-19 19:27:15 +0100

    Estructura base del proyecto y configuracion de Bootstrap

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserViewComponent } from './pages/user-view/user-view.component';
+import { UserFormComponent } from './pages/user-form/user-form.component';
+import { Error404Component } from './pages/error404/error404.component';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'user/:id', component: UserViewComponent },
+  { path: 'newuser', component: UserFormComponent },
+  { path: 'updateuser/:id', component: UserFormComponent },
+  { path: '**', component: Error404Component }
+];
---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { NgxSonnerToaster } from 'ngx-sonner';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, NgxSonnerToaster],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {}
